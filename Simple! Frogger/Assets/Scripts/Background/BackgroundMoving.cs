327e50266fcb943c2c2040eb6286d6b8f9a736aa
frankvega2010 <franco_vega@msn.com>
2019-08-04 17:19:29 -0300

    added simple water animation

---
+++ Simple! Frogger/Assets/Scripts/Background/BackgroundMoving.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMoving : MonoBehaviour
+{
+    public float speed;
+
+    private float timeBackground;
+    private MeshRenderer backgroundRenderer;
+    private void Start()
+    {
+        backgroundRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeBackground += Time.deltaTime;
+        backgroundRenderer.material.mainTextureOffset = new Vector2(timeBackground * speed, 0);
+    }
+}
